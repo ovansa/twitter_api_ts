b10e83de1a21fd5c358d5f66e61df213a1ff27de
Muhammed Ibrahim <aminmuhammad18@gmail.com>
2022-01-31 18:43:57 +0100

    Added api to get all tweets and to fetch all tweets created by self

--- src/controllers/tweet.controller.ts
+++ src/controllers/tweet.controller.ts
@@ -4,7 +4,7 @@ import { IAuthInfoRequest } from '../utils/definitions';
 import logger from '../utils/logger';
 
 // @desc    Add tweet
-// @route   POST /api/tweet/add
+// @route   POST /api/tweets
 // @access  Private
 export const addTweet = async (
   req: IAuthInfoRequest,
@@ -16,6 +16,44 @@ export const addTweet = async (
     return res.status(201).json({
       success: true,
       tweet,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+// @desc    Get all tweets
+// @route   GET /api/tweets
+// @access  Private
+export const getAllTweets = async (
+  req: IAuthInfoRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const tweets = await Tweet.find();
+    return res.status(201).json({
+      success: true,
+      tweets,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+// @desc    Get all tweets
+// @route   GET /api/tweets
+// @access  Private
+export const getAllTweetsCreatedBySelf = async (
+  req: IAuthInfoRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const tweets = await Tweet.find({ author_id: req.user._id });
+    return res.status(201).json({
+      success: true,
+      tweets,
     });
   } catch (err) {
     next(err);
--- src/routes/tweet.routes.ts
+++ src/routes/tweet.routes.ts
@@ -1,5 +1,9 @@
 import express from 'express';
-import { addTweet } from '../controllers/tweet.controller';
+import {
+  addTweet,
+  getAllTweets,
+  getAllTweetsCreatedBySelf,
+} from '../controllers/tweet.controller';
 import requireUser from '../middleware/auth';
 import validateResource from '../middleware/validateResource';
 import { addTweetSchema } from '../schema/tweet.schema';
@@ -7,5 +11,7 @@ import { addTweetSchema } from '../schema/tweet.schema';
 const router = express.Router();
 
 router.post('/add', requireUser, validateResource(addTweetSchema), addTweet);
+router.get('/', requireUser, getAllTweets);
+router.get('/self', requireUser, getAllTweetsCreatedBySelf);
 
 export default router;
